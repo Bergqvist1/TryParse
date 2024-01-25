f11ce21bdb29ce0b4c481c2e5f712311089e4e57
Bergqvist1 <emilsjostrand1000@gmail.com>
2024-01-25 12:32:15 +0100

    inte klar

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int resultat; 
+        if(!TryParse(Console.ReadLine(), out resultat))
+        {
+           Console.WriteLine("Skriv in en siffra"); 
+        }
+        else
+        {
+            Console.WriteLine(resultat);
+        }
+    }
+
+    public static bool TryParse(string s, out int resultat)
+    {
+        
+        try
+        {
+            resultat = int.Parse(s);
+            if(resultat != null)
+            {               
+                return true;
+            }
+        }
+        
+        catch
+        {
+            return false;
+        } 
+    }
+}
